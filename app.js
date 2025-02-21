4b17d22adb52a8c1f1abfb97e94f466ad216916f
mando <143672248+Sumandosilalahi2929@users.noreply.github.com>
2025-02-21 22:19:40 +0700

    feat: custom error

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 const app = express();
 
 //router
-const categoriesRouter = require('./app/api/v1/categories/router');
+const categoriesRouter = require("./app/api/v1/categories/router");
+//router
+const v1 = "/api/v1/cms";
 
-const v1 = '/api/v1/cms'
+const notFoundMiddleware = require("./app/middlewares/not-found");
+const handleErrorMiddleware = require("./app/middlewares/handle-error");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to world',
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to world",
+  });
 });
 
 app.use(v1, categoriesRouter);
