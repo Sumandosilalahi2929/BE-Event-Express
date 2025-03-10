cbdf96ea5ad1e3cb949f15deabf2bee676127b64
mando <143672248+Sumandosilalahi2929@users.noreply.github.com>
2025-03-10 15:55:42 +0700

    feat: api order and mock server

--- app/api/v1/events/router.js
+++ app/api/v1/events/router.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express();
 const { create, index, find, update, destroy } = require("./controller");
 
-router.get("/events", index);
-router.get("/events/:id", find);
-router.post("/events", create);
-router.put("/events/:id", update);
-router.delete("/events/:id", destroy);
+const {
+  authenticateUser,
+  authorizeRoles,
+} = require("../../../middlewares/auth");
+
+router.get("/events", authenticateUser, authorizeRoles("organizer"), index);
+router.get("/events/:id", authenticateUser, authorizeRoles("organizer"), find);
+router.post("/events", authenticateUser, authorizeRoles("organizer"), create);
+router.put(
+  "/events/:id",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  update
+);
+router.delete(
+  "/events/:id",
+  authenticateUser,
+  authorizeRoles("organizer"),
+  destroy
+);
 
 module.exports = router;
